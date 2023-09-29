1a0148ae3ff4ed1b10c0b616500e853d636b3d2c
WawinyEdwin <wawinyedwin44@gmail.com>
2023-09-29 09:06:04 +0300

    add email validation

--- src/auth/dto/password-request.dto.ts
+++ src/auth/dto/password-request.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty,  IsString } from "class-validator";
+import { IsEmail, IsNotEmpty } from "class-validator";
 
 export class PasswordRequestDto {
   @IsNotEmpty()
-  @IsString()
+  @IsEmail()
   readonly email: string;
 }
